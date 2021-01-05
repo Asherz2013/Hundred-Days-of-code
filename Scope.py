1ba7453e9c0f24ccd16e51ed482c27b3340393c9
Ashley <ashley.shaw2009@gmail.com>
2021-01-05 22:18:34 +0000

    Updated more knowledge about scope

--- Scope.py
+++ Scope.py
@@ -10,6 +10,9 @@ def increase_enemies():
     global enemies
     enemies += 1
     print(f"enemies inside function: {enemies}")
-    
+
 increase_enemies()
 print(f"enemies outside function: {enemies}")
+
+# General practice is to avoid modifying Global Scope variables, so the above is BAD!
+# A way around the above is to return the Global plus any modification that needs to take place.
