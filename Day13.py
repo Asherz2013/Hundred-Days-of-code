84ceeef43a043d8e77764ae0218c0e82efd00f00
Ashley <ashley.shaw2009@gmail.com>
2021-01-09 21:11:57 +0000

    New Challenge

--- Day13.py
+++ Day13.py
@@ -81,3 +81,13 @@
 * Run your code often. Means you are testing small changes
 * Look online - Maybe Stack Overflow
 """
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Challenges
+
+# 1.
+number = int(input("Which number do you want to check?"))
+if number % 2 = 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
