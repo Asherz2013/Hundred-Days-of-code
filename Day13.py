e56b96f427880075dc898b3037d7202c7cc03757
Ashley <ashley.shaw2009@gmail.com>
2021-01-09 21:29:21 +0000

    Challenge Solved

--- Day13.py
+++ Day13.py
@@ -127,13 +127,15 @@ Your program should print each number from 1 to 100 in turn.
 When the number is divisible by 3 then instead of printing the number it should print "Fizz".
 When the number is divisible by 5, then instead of printing the number it should print "Buzz".
 And if the number is divisible by both 3 and 5 e.g. 15 then instead of the number it should print "FizzBuzz"
-""" 
+"""
+# Problem - Divisible numbers by 3 are printing "FizzBuzz" AND "Fizz". Divisible numbers by 5 are printing "FizzBuzz" AND "Buzz". Random [] brackets round the numbers
+# Solution - All IF statements, need to be ELIF. First IF needs to be an AND rather than an OR. Remove the []
 for number in range(1, 101):
-  if number % 3 == 0 or number % 5 == 0:
+  if number % 3 == 0 and number % 5 == 0:
     print("FizzBuzz")
-  if number % 3 == 0:
+  elif number % 3 == 0:
     print("Fizz")
-  if number % 5 == 0:
+  elif number % 5 == 0:
     print("Buzz")
   else:
-    print([number])
+    print(number)
